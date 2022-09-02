5c406ebc982b706fedf41db368c5fea7c8495fae
jaysmitjadhav <jadhav.jaysmit@gmail.com>
2022-09-02 15:11:54 -0700

    created API in router.js

---
+++ server/controller/controller.js
@@ -0,0 +1,21 @@
+var Userdb = require('../model/model');
+
+// create and save new user
+exports.create = (req, res) => {
+
+}
+
+// retrive and return single/all user/s
+exports.find = (req, res) => {
+
+}
+
+// update user 
+exports.update = (req, res) => {
+
+}
+
+// delete user
+exports.delete = (req, res) => {
+    
+}
